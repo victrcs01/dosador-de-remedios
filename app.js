de35128dc245fe7ceb974abe80bc293fcb54d936
victrcs01 <88691506+victrcs01@users.noreply.github.com>
2023-11-28 12:59:32 -0300

    atualiza a porta do servidor

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/remedies', remedyRoutes);
 // Rotas para o gerenciamento do dispenser dos medicamentos
 app.use('/dispenser', dispenserRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
