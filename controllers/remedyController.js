b4a78c3bb784ec2744e34b4b6b48bcccfbdd56ea
victrcs01 <88691506+victrcs01@users.noreply.github.com>
2023-12-07 22:11:43 -0300

    Update remedyController.js

--- controllers/remedyController.js
+++ controllers/remedyController.js
@@ -18,7 +18,7 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const remedy = await prisma.remedy.findUnique({
-      where: { remedy_id: parseInt(id) },
+      where: { id: parseInt(id) },
     });
     if (!remedy) {
       return res.status(404).json({ error: error });
@@ -31,15 +31,15 @@ router.get('/:id', async (req, res) => {
 
 // Criar um novo Remedy
 router.post('/', async (req, res) => {
-  const { name, doses_frequency, amount_per_dose, remedy_notes, tube_identifier, remaining_doses, last_dose } = req.body;
+  const { name, frequency, amount_per_dose, notes, tube_identifier, remaining_doses, last_dose } = req.body;
   try {
     //const last_dose = '2023-11-28 15:30:00';
     const newRemedy = await prisma.remedy.create({
       data: {
         name,
-        doses_frequency,
+        frequency,
         amount_per_dose, 
-        remedy_notes, 
+        notes, 
         tube_identifier: Number(tube_identifier),
         last_dose,
         remaining_doses
@@ -55,15 +55,15 @@ router.post('/', async (req, res) => {
 // Atualizar um Remedy por ID
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
-  const { name, doses_frequency, amount_per_dose, remedy_notes, tube_identifier } = req.body;
+  const { name, frequency, amount_per_dose, notes, tube_identifier } = req.body;
   try {
     const updatedRemedy = await prisma.remedy.update({
-      where: { remedy_id: parseInt(id) },
+      where: { id: parseInt(id) },
       data: {
         name,
-        doses_frequency,
+        frequency,
         amount_per_dose, 
-        remedy_notes, 
+        notes, 
         tube_identifier: Number(tube_identifier),
         last_dose,
         remaining_doses
@@ -80,7 +80,7 @@ router.delete('/:id', async (req, res) => {
   const { id } = req.params;
   try {
     await prisma.remedy.delete({
-      where: { remedy_id: parseInt(id) },
+      where: { id: parseInt(id) },
     });
     res.json({ message: 'Medicamento deletado com sucesso' });
   } catch (error) {
